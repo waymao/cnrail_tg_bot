6af32773b7ac53a161088ec48383e665fc887f6e
waymao <my.waymao@gmail.com>
2019-12-27 14:32:38 +0800

    merge changes from the server; clean up

--- commands/history.py
+++ commands/history.py
@@ -9,16 +9,18 @@ import pytz
 
 tz = pytz.timezone('Asia/Shanghai')
 
-{'train_no': 'G7501',
- 'train_id': '3776',
- 'train_type': 'CRH380BL',
- 'train_date': '2019-06-14',
- 'company': '上海局'}
+
+# Sample data:
+#{'train_no': 'G7501',
+# 'train_id': '3776',
+# 'train_type': 'CRH380BL',
+# 'train_date': '2019-06-14',
+# 'company': '上海局'}
+#
 # General function for formatting a message.
 # input: train_list, the list passed from the get_mysql function.
 #        no_first, whether we put the number first or not.
 # output: formatted string to be sent to the client.
-# TODO: Completely seperate the make message functions for train no and reg.
 def make_message(train_list, no_first):
     # t = PrettyTable(['NO', 'Registration', 'Type', 'Date', 'Company'])
     if no_first:
@@ -29,35 +31,14 @@ def make_message(train_list, no_first):
                       train_list['train_date'],
                       train_list['train_no'])
     else:
-        text = make_trainno_msg(train_list)
-    return text
-
-
-# Special function for formatting a message for train model and registration
-#     query.
-# input: train_list, the list passed from the get_mysql function.
-# output: formatted string to be sent to the client.
-def make_trainno_msg(train_list):
-    # two trains running on the same day, i.e. chonglian
-    if train_list[0]['train_date'] == train_list[1]['train_date']:
-        text = "**{}**车次，由{} **{}-{}** 和 **{}-{}**重联， 最近一次于 **{}** 执行。".format(
-            train_list[0]['train_no'], 
-            train_list[0]['company'],
-            train_list[0]['train_type'],
-            train_list[0]['train_id'],
-            train_list[1]['train_type'],
-            train_list[1]['train_id'],
-            train_list[0]['train_date']
-        )
-    else:
         text = "**{}**车次，由{} **{}-{}**， 最近一次于 **{}** 执行。".format(
-            train_list[0]['train_no'], 
-            train_list[0]['company'],
-            train_list[0]['train_type'],
-            train_list[0]['train_id'],
-            train_list[0]['train_date']
+            train_list['train_no'], 
+            train_list['company'],
+            train_list['train_type'],
+            train_list['train_id'],
+            train_list['train_date']
         )
-    return text
+    return text + "\n"
 
 
 # Handling function for train_no query
@@ -84,6 +65,7 @@ def train_no(bot, update, args):
         bot.edit_message_text(chat_id=update.message.chat_id, text="Your query did not return any results.", message_id = msg.message_id)
     else:
         bot.edit_message_text(chat_id=update.message.chat_id, text=make_message(result, True) + data_link_msg, parse_mode=telegram.ParseMode.MARKDOWN, message_id = msg.message_id)
+
 
 # Handling function for train_registration query
 def train_info(bot, update, args):
